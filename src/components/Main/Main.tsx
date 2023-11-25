b7d5f730065e3745afec0819446fe11b1640968e
Juan Pablo Azambuyo <juanpabloazambuyo@gmail.com>
2023-11-25 01:32:15 -0300

    done random AI with game end checker

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,12 +1,11 @@
 import css from './MainCSS.module.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { useNavigate } from "react-router-dom";
 import { Button } from '@mui/material/';
 import { useDispatch, useSelector } from 'react-redux';
 import Swal from 'sweetalert2';
 
 const Main = () => {
-  console.log("AAAAAAAAAAAAAAAAAAAAAA")
 
   const [ rowsAndColumns, setRowsAndColumns ] = useState<any[]>(
     [
@@ -14,21 +13,53 @@ const Main = () => {
       {checked: false, value: ''}, {checked: false, value: ''}, {checked: false, value: ''},
       {checked: false, value: ''}, {checked: false, value: ''}, {checked: false, value: ''}
     ]
-
   )
 
-  // let rowsAndColumns = [
-  //   {checked: false, value: ''}, {checked: false, value: ''}, {checked: false, value: ''},
-  //   {checked: false, value: ''}, {checked: false, value: ''}, {checked: false, value: ''},
-  //   {checked: false, value: ''}, {checked: false, value: ''}, {checked: false, value: ''}
-  // ]
+  let IAvalue = useRef(Math.floor(Math.random() * 9))
 
   const clickHandler = ({ target }: any) => {
-    console.log("target", target)
+
     let copyRowsAndColumns = [...rowsAndColumns]
-    copyRowsAndColumns[target].value = "X"
-    setRowsAndColumns(copyRowsAndColumns)
+
+    if (copyRowsAndColumns[target].value === "") {
+      rowsAndColumns[target].value = "X"
+      setRowsAndColumns(copyRowsAndColumns)
+
+
+      /* BEGIN RANDOM IA SECTION */
+      if (copyRowsAndColumns.filter(e => e.value === '').length > 1) {
+        if (copyRowsAndColumns[IAvalue.current].value === "") {
+          copyRowsAndColumns[IAvalue.current].value = "O"
+          setRowsAndColumns(copyRowsAndColumns)
+        } else {
+          let success = false
+          do {
+            IAvalue.current = Math.floor(Math.random() * 9)
+            if (copyRowsAndColumns[IAvalue.current].value === "") {
+              copyRowsAndColumns[IAvalue.current].value = "O"
+              setRowsAndColumns(copyRowsAndColumns)
+              success = true
+            }
+          } while (copyRowsAndColumns[IAvalue.current].value !== "" && success === false)
+        }
+      } 
+      /* END RANDOM IA SECTION */
+
+
+    }
+      
+
+    
+
+    console.log("123 filter(e => e.value === '').length", copyRowsAndColumns.filter(e => e.value === '').length)
+
+    if (copyRowsAndColumns.filter(e => e.value === '').length === 0) {
+      console.log("123 GAME END")
+    }
+
   }
+
+  console.log("123 actual", rowsAndColumns)
 
   return (
     <div className={css.background}>
@@ -41,20 +72,21 @@ const Main = () => {
           rowsAndColumns.map((e, index) => {
             return (
               <div
+                key={index}
                 id={`${index}`}
                 onClick={(e) => { clickHandler({ target: index }) }}
                 className={css.eachBox}
               >
                 {/* {rowsAndColumns[parseInt((e.target as HTMLInputElement).id, 10)].value} */}
                 { rowsAndColumns[index].value }
-              </div> 
+              </div>
             )
           })
         }
       </div>
-     
 
-      
+
+
     </div>
   );
 }
