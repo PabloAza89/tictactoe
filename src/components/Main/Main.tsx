aa9d09ca9924955f5df01933c6aa7f6bcf84d47f
Juan Pablo Azambuyo <juanpabloazambuyo@gmail.com>
2023-11-24 23:11:14 -0300

    commit 4

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,18 @@
+import css from './MainCSS.module.css';
+import { useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
+import { Button } from '@mui/material/';
+import { useDispatch, useSelector } from 'react-redux';
+import Swal from 'sweetalert2';
+
+const Main = () => {
+  console.log("AAAAAAAAAAAAAAAAAAAAAA")
+  return (
+    <div className={css.background}>
+      ASDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD
+      
+    </div>
+  );
+}
+
+export default Main;
--- src/index.tsx
+++ src/index.tsx
@@ -5,17 +5,14 @@ import App from './App';
 import  { Provider } from 'react-redux';
 import store from './store/store';
 import { BrowserRouter } from 'react-router-dom';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.render(
-  <GoogleOAuthProvider clientId={`${process.env.REACT_APP_GOAP}`}>
     <React.StrictMode>
       <Provider store={store}>
-        <BrowserRouter basename="/PI-Food-GH">
+        <BrowserRouter basename="/tictactoe">
           <App />
         </BrowserRouter>
       </Provider>
-    </React.StrictMode>
-  </GoogleOAuthProvider>,
+    </React.StrictMode>,
   document.getElementById('root')
 );
