0c8e7510956180f3237eba9b0cfed6162e3b1dc7
Juan Pablo Azambuyo <juanpabloazambuyo@gmail.com>
2023-11-25 00:11:43 -0300

    basic structure done

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,5 +1,5 @@
 import css from './MainCSS.module.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import { Button } from '@mui/material/';
 import { useDispatch, useSelector } from 'react-redux';
@@ -7,9 +7,53 @@ import Swal from 'sweetalert2';
 
 const Main = () => {
   console.log("AAAAAAAAAAAAAAAAAAAAAA")
+
+  const [ rowsAndColumns, setRowsAndColumns ] = useState<any[]>(
+    [
+      {checked: false, value: ''}, {checked: false, value: ''}, {checked: false, value: ''},
+      {checked: false, value: ''}, {checked: false, value: ''}, {checked: false, value: ''},
+      {checked: false, value: ''}, {checked: false, value: ''}, {checked: false, value: ''}
+    ]
+
+  )
+
+  // let rowsAndColumns = [
+  //   {checked: false, value: ''}, {checked: false, value: ''}, {checked: false, value: ''},
+  //   {checked: false, value: ''}, {checked: false, value: ''}, {checked: false, value: ''},
+  //   {checked: false, value: ''}, {checked: false, value: ''}, {checked: false, value: ''}
+  // ]
+
+  const clickHandler = ({ target }: any) => {
+    console.log("target", target)
+    let copyRowsAndColumns = [...rowsAndColumns]
+    copyRowsAndColumns[target].value = "X"
+    setRowsAndColumns(copyRowsAndColumns)
+  }
+
   return (
     <div className={css.background}>
-      ASDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD
+      <div className={css.crossCircle}>
+        <div className={css.cross}>X</div>
+        <div className={css.circle}>O</div>
+      </div>
+      <div className={css.rowsAndColumns}>
+        {
+          rowsAndColumns.map((e, index) => {
+            return (
+              <div
+                id={`${index}`}
+                onClick={(e) => { clickHandler({ target: index }) }}
+                className={css.eachBox}
+              >
+                {/* {rowsAndColumns[parseInt((e.target as HTMLInputElement).id, 10)].value} */}
+                { rowsAndColumns[index].value }
+              </div> 
+            )
+          })
+        }
+      </div>
+     
+
       
     </div>
   );
